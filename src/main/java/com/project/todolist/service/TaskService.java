862d74ce664aef4ca80148d825bdd3efb8b6a26d
José Thomaz <jose.thoomaz@gmail.com>
2025-09-19 00:22:46 -0300

    create service

---
+++ src/main/java/com/project/todolist/service/TaskService.java
@@ -0,0 +1,46 @@
+package com.project.todolist.service;
+
+import com.project.todolist.model.Task;
+import com.project.todolist.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long  id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task updateTask(Long id, Task updateTask) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updateTask.getTitle());
+                    task.setDescription(updateTask.getDescription());
+                    task.setPriority(updateTask.getPriority());
+                    return taskRepository.save(task);
+                })
+                .orElseThrow(() -> new RuntimeException("Task not found with id " + id));
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
